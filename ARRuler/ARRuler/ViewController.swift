631bf0cbbbc356baf7db72297220c79c627fcf55
Hoang Vo <hoangvo@gmail.com>
2017-10-07 10:38:27 -0500

    feature: AR Ruler

---
+++ ARRuler/ARRuler/ViewController.swift
@@ -0,0 +1,131 @@
+//
+//  ViewController.swift
+//  ARRuler
+//
+//  Created by Hoang Vo on 10/7/17.
+//  Copyright © 2017 Hoang Vo. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    var dotNodes = [SCNNode]()
+    var textNode = SCNNode()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if dotNodes.count >= 2 {
+            for dot in dotNodes {
+                dot.removeFromParentNode()
+            }
+            dotNodes = [SCNNode]()
+        }
+        
+        if let touchLocation = touches.first?.location(in: sceneView) {
+            let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint)
+            
+            if let hitResult = hitTestResults.first {
+                addDot(at: hitResult)
+            }
+        }
+        
+    }
+    
+    func addDot(at hitResult: ARHitTestResult) {
+        let dotGeometry = SCNSphere(radius: 0.005)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        
+        dotGeometry.materials = [material]
+        
+        let dotNode = SCNNode(geometry: dotGeometry)
+        
+        dotNode.position = SCNVector3(x: hitResult.worldTransform.columns.3.x, y: hitResult.worldTransform.columns.3.y, z: hitResult.worldTransform.columns.3.z)
+        
+        sceneView.scene.rootNode.addChildNode(dotNode)
+        
+        dotNodes.append(dotNode)
+        
+        if dotNodes.count >= 2 {
+            calculate()
+        }
+    }
+    
+    func calculate() {
+        let start = dotNodes[0]
+        let end = dotNodes[1]
+        
+        // https://technology.cpm.org/general/3dgraph/
+        // distance = sqrt(a^2 + b^2 + c^2)
+        print(start.position)
+        print(end.position)
+        
+        let a = end.position.x - start.position.x
+        let b = end.position.y - start.position.y
+        let c = end.position.z - start.position.z
+        
+        // take the alt. pythagorean theorum
+        let d = sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2))
+        
+        // unit by default is in meters, converting to inches by converting first to centimeters
+        let dInches = centimetersToInches(depthInCentimeters: d*100)
+        print("distance: \(abs(d)), \(abs(dInches))")
+        updateText(text: "\(abs(dInches)) in.", atPosition: end.position)
+    }
+    
+    func updateText(text: String, atPosition position: SCNVector3) {
+        
+        textNode.removeFromParentNode()
+        
+        let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+        
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.red
+        
+        textNode = SCNNode(geometry: textGeometry)
+        
+        textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
+        
+        // reduces the size of 3D text to .1% of text on all 3 dimensions
+        textNode.scale = SCNVector3(0.001, 0.001, 0.001)
+        
+        sceneView.scene.rootNode.addChildNode(textNode)
+    }
+    
+    func centimetersToInches(depthInCentimeters: Float) -> Float {
+        let depthInInches = depthInCentimeters / 2.54
+        return depthInInches as Float
+    }
+}
